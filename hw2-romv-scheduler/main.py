0378f0f3b58ec8b0d2e358f0d886b30e20828a73
Elad Nachmias <eladnah@gmail.com>
2018-05-09 16:58:22 +0300

    main - argparse - sched from choices

--- hw2-romv-scheduler/main.py
+++ hw2-romv-scheduler/main.py
@@ -5,14 +5,17 @@ from scheduler_modules import ROMVScheduler, SerialScheduler
 
 
 def args_parser():
-    parser = argparse.ArgumentParser(description='Grabber for Zira.ninja (sdarot.tv) videos.')
-    parser.add_argument('--sched', '-s', type=str, nargs='?', help='Scheduler to use. `RR` for round-robin, or `serial` for serial scheduler.', default='RR')
+    parser = argparse.ArgumentParser(
+        description='DB implementation [236510] / Simulator for ROMV transactions scheduler.')
+    parser.add_argument('--sched', '-s',
+                        type=str, nargs='?', choices=['RR', 'serial'], default='RR',
+                        help='Scheduler to use. `RR` for round-robin, or `serial` for serial scheduler.')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = args_parser()
-    assert args.sched in {'RR', 'serial'}  # TODO: check it and print friendly informative error.
+    assert args.sched in {'RR', 'serial'}
     scheduler_type = ROMVScheduler if args.sched == 'RR' else SerialScheduler
 
     scheduler = scheduler_type()
