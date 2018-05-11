51f8e76d636a819b6f9e117690f38e3dda8e956b
Elad Nachmias <eladnah@gmail.com>
2018-05-12 01:49:20 +0300

    NoFalseNegativeVariablesSet impl

--- hw2-romv-scheduler/scheduler_base_modules.py
+++ hw2-romv-scheduler/scheduler_base_modules.py
@@ -279,15 +279,31 @@ class Scheduler(ABC):
 
 
 # Notice: we could use a bloom-filter here. # TODO: elaborate on it.
-class VariablesSet:
+class NoFalseNegativeVariablesSet:
     def __init__(self):
-        pass  # TODO: impl
+        self._variables_set = set()
 
-    def add_variable(self):
-        pass  # TODO: impl
+    def add_variable(self, variable):
+        self._variables_set.add(variable)
 
-    def is_variable_exists(self):
-        pass  # TODO: impl
+    def add_variables(self, variables_set_to_add):
+        assert isinstance(variables_set_to_add, NoFalseNegativeVariablesSet)
+        self._variables_set = self._variables_set.union(variables_set_to_add._variables_set)
 
-    def intersect(self, another_variables_set):
-        pass  # TODO: impl
+    def is_variable_exists(self, variable):
+        return variable in self._variables_set
+
+    def intersection(self, another_variables_set):
+        assert isinstance(another_variables_set, NoFalseNegativeVariablesSet)
+        new_variables_set = NoFalseNegativeVariablesSet()
+        new_variables_set._variables_set = self._variables_set.intersection(another_variables_set._variables_set)
+        return new_variables_set
+
+    def difference(self, another_variables_set):
+        assert isinstance(another_variables_set, NoFalseNegativeVariablesSet)
+        new_variables_set = NoFalseNegativeVariablesSet()
+        new_variables_set._variables_set = self._variables_set.difference(another_variables_set._variables_set)
+        return new_variables_set
+
+    def __iter__(self):
+        return iter(self._variables_set)
