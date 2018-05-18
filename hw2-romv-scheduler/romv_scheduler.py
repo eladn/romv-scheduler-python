49a5715917f7b699b6a4d42d52d89efd9a4e39d0
Elad Nachmias <eladnah@gmail.com>
2018-05-18 11:06:38 +0300

    ROMVTransaction+UMVTransaction comments

--- hw2-romv-scheduler/romv_scheduler.py
+++ hw2-romv-scheduler/romv_scheduler.py
@@ -67,6 +67,12 @@ class ROMVTransaction(Scheduler.ROTransaction):
     def __init__(self, *args, **kargs):
         kargs['is_read_only'] = True
         super().__init__(*args, **kargs)
+        # Each ongoing read-only transaction holds a set of variables that has been committed
+        # (by an update transaction) before that reader has born, but after the youngest older
+        # reader has been born. So that, in each interval of time between the birth of each pair
+        # of consecutive read-only transactions, the youngest one holds a set of all the variables
+        # that has been committed in that interval of time. This makes that reader "responsible"
+        # for these versions in some sense. More about it in the GC mechanism explanation.
         self._committed_variables_set_since_last_reader_born = NoFalseNegativeVariablesSet()
 
     @property
@@ -84,6 +90,10 @@ class UMVTransaction(Scheduler.UTransaction):
         kargs['is_read_only'] = False
         super().__init__(*args, **kargs)
         self._local_written_values = dict()  # TODO: is it ok to store them on memory only?
+        # For each variable that the transaction updates, we store the previous version
+        # of that variable. This data is needed for the GC mechanism. In order to know
+        # which versions to evict, in a case where there is no reader that is "responsible"
+        # for the previous version.
         self._committed_variables_latest_versions_before_update = None
 
     @property
@@ -91,6 +101,8 @@ class UMVTransaction(Scheduler.UTransaction):
         assert self._committed_variables_latest_versions_before_update is not None
         return self._committed_variables_latest_versions_before_update
 
+    # Called by the scheduler after the transaction commits, in order to store to
+    # disk the updates that have been made by this transaction, as new versions.
     def complete_writes(self, mv_data_manager: MultiVersionDataManager):
         self._committed_variables_latest_versions_before_update = {
             variable: mv_data_manager.get_latest_version_number(variable)
@@ -99,6 +111,8 @@ class UMVTransaction(Scheduler.UTransaction):
         for variable, value in self._local_written_values.items():
             mv_data_manager.write_new_version(variable, value, self.timestamp)
 
+    # Updates that the transaction makes are accessible only for this transaction.
+    # Hence, we store them locally on the transacting session instance.
     def local_write(self, variable, value):
         self._local_written_values[variable] = value
 
