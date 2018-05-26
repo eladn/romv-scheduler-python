b6ed5f06b56eb2be45a2ad377630582d655af5ad
Elad Nachmias <eladnah@gmail.com>
2018-05-26 17:17:20 +0300

    doc: scheduler interface

--- hw2-romv-scheduler/scheduler_interface.py
+++ hw2-romv-scheduler/scheduler_interface.py
@@ -6,12 +6,12 @@ from itertools import chain
 
 # This is a pure-abstract class. It is inherited later by the `ROMVScheduler` and the `SerialScheduler`.
 # The `SchedulerInterface` include basic generic methods for storing the transactions in lists and iterating
-# over them. The `SchedulerInterface` defined an interface, so that any actual scheduler that inherits from
-# `SchedulerInterface` must conform with that interface. Any scheduler must implement the methods:
+# over them. The `SchedulerInterface` is defined an interface, so that any actual scheduler that inherits from
+# `SchedulerInterface` must conform with that interface. Any scheduler must implement the following methods:
 #   run()
 #       It iterates over that transactions and tries to perform their first awaiting operation.
-#       The method `iterate_over_transactions_by_tid_and_safely_remove_marked_to_remove_transactions()`
-#       of the scheduler might be handy here.
+#       The method `iterate_over_ongoing_transactions_and_safely_remove_marked_to_remove_transactions()`
+#       of the scheduler might be handy for the implementation of this method.
 #   try_write(transaction_id, variable, value)
 #       When the scheduler tries to perform a "write" operation (of a transaction), the operation
 #       receives a pointer to the scheduler. The method `operation.try_perform(scheduler)` might
@@ -85,23 +85,39 @@ class SchedulerInterface(ABC):
         # the mechanism to notify the inheritor scheduler that a transaction has been added.
         self.on_add_transaction(transaction)
 
-    # TODO: doc!
+    # Part of the mechanism for transaction removal while iteration.
+    # We cannot remove a transaction while iterating over the transactions list.
+    # Hence, we just `mark` the transaction for removal while iterating.
+    # We actually remove the marked-to-remove transactions from the transactions
+    # list only after the end of the iteration.
+    # The marking to removal is by adding the transaction to a dedicated list.
+    # We also remove the transaction from the mapping here.
     def mark_transaction_to_remove(self, transaction: Transaction):
         assert transaction.transaction_id in self._ongoing_transactions_mapping
         del self._ongoing_transactions_mapping[transaction.transaction_id]
         self._to_remove_transactions.push_back(transaction)
 
-    # TODO: doc!
+    # Part of the mechanism for transaction removal while iteration.
+    # Iterate over the list of marked-to-remove transactions, and perform the
+    # removal of each one of these.
     def remove_marked_to_remove_transactions(self):
         for transaction_to_remove in self._to_remove_transactions:
             self.remove_transaction(transaction_to_remove)
         self._to_remove_transactions.clear()
 
-    # TODO: doc!
+    # Actual full removal of a transaction from the scheduler.
+    # Don't use it while iterating over the transactions list
+    # (in that case use `mark_transaction_to_remove()` instead).
     def remove_transaction(self, transaction_to_remove: Transaction):
         self._ongoing_transactions_by_tid.remove_node(transaction_to_remove.transactions_by_tid_list_node)
 
-        # TODO: doc the following check.
+        # When a transaction is aborted, the user callback is balled. The user then might "reset" the transaction.
+        # That is, the user might create and add a new transaction with the same transaction-id to the scheduler.
+        # This creates a problem because the scheduler marks the aborted transaction to be removed.
+        # Because of the mechanism for transaction removal while iteration, the actual removal will take place
+        # only after the new transaction has been added by the user. This new added transaction might have the
+        # same transaction-id as the already-marked-to-be-removed aborted transaction. Hence, we check here whether
+        # there exists a new transaction with the same id. In that case, we do not remove it from the mapping.
         if transaction_to_remove.transaction_id in self._ongoing_transactions_mapping and \
                 self._ongoing_transactions_mapping[transaction_to_remove.transaction_id] is transaction_to_remove:
             self._ongoing_transactions_mapping.pop(transaction_to_remove.transaction_id)
@@ -122,7 +138,7 @@ class SchedulerInterface(ABC):
             prev_transaction = transaction
         return prev_transaction
 
-    # This is a generator function. It returns an iterator. The returned iterator yields a
+    # This is a generator function. It returns an iterator. The returned iterator yields
     # a transaction in each iteration. The iterator is fulfilled when no more ongoing
     # transactions exist. The iteration scheme can be either 'RR' (round-robin) based, or
     # serial based. It is determined by the scheduling-scheme that is set in the c'tor.
@@ -153,6 +169,12 @@ class SchedulerInterface(ABC):
             # of each outer loop we remove all of the transaction that are marked to be removed.
             self.remove_marked_to_remove_transactions()
 
+    # This is a generator function. It returns an iterator. The returned iterator yields
+    # a transaction in each iteration, by the given order `forced_run_order`.
+    # The parameter `forced_run_order` is an iterable sequence that each element is the
+    # transaction id of the transaction to run next. Each transaction is being yielded
+    # over-and-over again until it is completed. We assume this is used with the serial
+    # scheduling scheme.
     def iterate_over_ongoing_transactions_by_given_order_and_safely_remove_marked_to_remove_transactions(
             self, forced_run_order):
         assert forced_run_order is not None
@@ -165,9 +187,11 @@ class SchedulerInterface(ABC):
                 yield transaction
                 self.remove_marked_to_remove_transactions()
 
+    # Return one of the iterators above, based on whether the parameter `forced_run_order` is given.
     def iterate_over_ongoing_transactions_and_safely_remove_marked_to_remove_transactions(self, forced_run_order=None):
         if forced_run_order is None:
             return self.iterate_over_ongoing_transactions_by_tid_and_safely_remove_marked_to_remove_transactions()
+        assert self._scheduling_scheme == 'serial'
         return self.iterate_over_ongoing_transactions_by_given_order_and_safely_remove_marked_to_remove_transactions(
             forced_run_order)
 
@@ -179,33 +203,43 @@ class SchedulerInterface(ABC):
     def get_serialization_order(self):
         return iter(self._serialization_order)
 
+    # Being raised by the following method.
     class NotEqualException(ValueError):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
 
+    # Given any number of schedulers as arguments, compare the latest values of each variable of these schedulers.
+    # Firstly, compare that all of these schedulers have exactly the same variables names.
+    # Secondary, for each variable, verify that all of the schedulers have the same latest version for that variable.
     @staticmethod
     def compare_results(*schedulers):
         assert all(isinstance(scheduler, SchedulerInterface) for scheduler in schedulers)
         assert len(schedulers) > 1
 
+        # list that each element is the {variables: values} dict of a given scheduler.
         all_values_of_all_schedulers = list(dict(scheduler.get_variables()) for scheduler in schedulers)
 
+        # list that each element is the set of variables names of a given scheduler.
         variable_names_per_scheduler = list(set(item[0] for item in variables_of_scheduler)
                                             for variables_of_scheduler in all_values_of_all_schedulers)
-        varialbes_names_union = set(chain(*variable_names_per_scheduler))
+        # set of all variable names that occurred in a scheduler.
+        variables_names_union = set(chain(*variable_names_per_scheduler))
 
-        if not all(varialbes_names_union == variable_names_of_scheduler
+        # Verify that all of the schedulers have the same variable names.
+        if not all(variables_names_union == variable_names_of_scheduler
                    for variable_names_of_scheduler in variable_names_per_scheduler):
             raise SchedulerInterface.NotEqualException(
                 'Not all schedulers have the same variable names.')
 
         # Verify that for each variable, all of the schedulers have the same value for that variable.
         for variable_name in variable_names_per_scheduler[0]:
+            # set of the latest values that the current variable got in the schedulers.
             values = set(variables_of_scheduler[variable_name][-1][0]  # take the value of the last version
                          if isinstance(variables_of_scheduler[variable_name], list)
                          else variables_of_scheduler[variable_name]
                          for variables_of_scheduler
                          in all_values_of_all_schedulers)
+            # all of the schedulers got the same latest value for `variable` iff the values set is of size 1.
             if len(values) != 1:
                 raise SchedulerInterface.NotEqualException(
                     'Not all schedulers have the same values of variable {variable}.'.format(variable=variable_name))
